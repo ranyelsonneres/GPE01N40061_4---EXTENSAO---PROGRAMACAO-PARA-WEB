ee2975130430857673013fe47888439f2435ee22
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-10-27 20:47:39 -0300

    Update api.js

--- Projeto-Crud/routes/api.js
+++ Projeto-Crud/routes/api.js
@@ -1,1 +1,24 @@
-//lógica do backend
+//importar o express
+const express = require('express');
+
+//middleware (recebe e processa a requisição e encaminha)
+const router = express.Router();
+
+//trazer a conexão do banco de dados
+const db = require('../db');
+
+//criar as rotas
+//cadastrar um usuário: /api/users/
+router.post('/', (req, res) =>{
+    const {nome, email} = req.body;
+
+    //executar a instrução SQL
+    db.query('INSERT INTO users (nome, email) VALUES (?, ?)', [nome, email], 
+        (err, result) =>{
+            if (err) return res.status(500).send(err);
+            res.status(201).json({id: result.insertId, nome, email});
+    });
+});
+
+//exportar as rotas
+module.exports = router;
