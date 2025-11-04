ba7d1b007f537d15b0e866de4f1eb53c3627ab8e
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-03 21:40:33 -0300

    Update app.js

--- Projeto-Crud/public/app.js
+++ Projeto-Crud/public/app.js
@@ -1,1 +1,29 @@
-//logica do front
+const form = document.getElementById('user-form'); //trazer o formulário do cliente
+
+//função para tratar os eventos do meu formulário
+form.addEventListener('submit', e=>{
+    //evitar que a página seja recarregada
+    e.preventDefault();
+    
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+
+    //chamar a função para cadastrar
+    cadastrarUsuario(nome, email);
+})
+
+//função para cadastrar
+function cadastrarUsuario(nome, email){
+    //fetch() - requisição para o servidor HTTP 
+    //POST, PUT, DELETE, GET, PATCH
+    //endpoint (/api/users/) - rota para o nosso servidor
+    fetch('/api/users/', {
+        method: 'POST',
+        headers: { 'Content-Type':'application/json'},
+        body: JSON.stringify({nome, email})
+    })
+    .then(() =>{
+        //limpar o formulário
+        form.reset();
+    })
+}
