08fcaf110b5b2c6c31dd5f0979578d077aef2852
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-11 16:09:35 -0300

    Update app.js

--- Projeto-Crud/public/app.js
+++ Projeto-Crud/public/app.js
@@ -1,5 +1,9 @@
 const form = document.getElementById('user-form'); //trazer o formulário do cliente
 
+const userList = document.getElementById('user-list');
+
+carregarUsuarios(); //carregar os usuários cadastrados no banco
+
 //função para tratar os eventos do meu formulário
 form.addEventListener('submit', e=>{
     //evitar que a página seja recarregada
@@ -25,5 +29,53 @@ function cadastrarUsuario(nome, email){
     .then(() =>{
         //limpar o formulário
         form.reset();
+        carregarUsuarios();
+    })
+}
+
+//função para carregar os usuários do Banco de Dados
+function carregarUsuarios(){
+    //requisição via fetch
+    fetch('/api/users/')
+    .then(res => res.json())
+    .then(data =>{
+        userList.innerHTML = '';
+        data.forEach(user =>{
+            const li = document.createElement('li');
+            li.innerHTML = `${user.nome} (${user.email}) 
+                <button onclick="atualizarUsuario(${user.id})">Editar</button>
+                <button onclick="excluirUsuario(${user.id})">Excluir</button>`;
+                userList.append(li);
+        })
+    })
+}
+
+//função para atualizar
+function atualizarUsuario(id) {
+    const nome = prompt('Digite o novo nome:');
+    const email = prompt('Digite o novo email:');
+    fetch(`/api/users/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nome, email })
+    })
+    .then(() => {
+        carregarUsuarios();
+    })
+}
+
+//função para excluir
+function excluirUsuario(id) {
+    const confirmacao = confirm('Tem certeza que deseja excluir este usuário?');
+
+    if (!confirmacao) {
+        return;
+    }
+
+    fetch(`/api/users/${id}`, {
+        method: 'DELETE'
+    })
+    .then(() => {
+        carregarUsuarios();
     })
 }
